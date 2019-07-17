0312c057fd7febdacd0a4c636a00332781a1b549
EvanMu96 <senmu2-c@my.cityu.edu.hk>
2019-07-17 14:47:19 +0800

    remove focal loss and hand written l2 regularization, use built in

--- experiments.py
+++ experiments.py
@@ -44,29 +44,6 @@ def train_softmax(model, criterion, optimizer, trainloader, use_gpu, epoch, num_
             print(dt(), 'epoch=%d batch#%d batchloss=%.4f averLoss=%.4f'
                     % (epoch, batch_idx, loss.item(), trainloss / (batch_idx + 1)))
 
-def train_softmax_l2(model, criterion, optimizer, trainloader, use_gpu, epoch, num_classes):
-    model.train()
-    model.feature = False
-    trainloss = 0
-    l2loss = 0
-    for batch_idx, (data, labels) in enumerate(trainloader):
-        if use_gpu:
-            data, labels = data.cuda(), labels.cuda()
-
-        _, outputs = model(data)
-        for p in model.parameters():
-            l2loss += p.abs().sum()
-        loss = criterion(outputs, labels) + l2loss
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        trainloss += loss.item() 
-        
-        if batch_idx % 10 == 0:
-            print(dt(), 'epoch=%d batch#%d batchloss=%.4f averLoss=%.4f'
-                    % (epoch, batch_idx, loss.item(), trainloss / (batch_idx + 1)))
-
 def train_centerloss(model, criterion_xent, criterion_cent, optimizer_model, optimizer_centloss, trainloader, use_gpu, epoch, coeff=0.005):
     model.train()
     model.feature = False
@@ -163,69 +140,7 @@ def train_sphere(model, criterion_sphere, optimizer_model, trainloader, use_gpu,
         if batch_idx % 10 == 0:
             print(dt(), 'epoch=%d batch#%d batchloss=%.4f averLoss=%.4f'
                     % (epoch, batch_idx, loss.item(), trainloss / (batch_idx + 1)))
-"""
-Focal Loss proposed by Kaiming He
-"""
-class FocalLoss(nn.Module):
-    r"""
-        This criterion is a implemenation of Focal Loss, which is proposed in 
-        Focal Loss for Dense Object Detection.
 
-            Loss(x, class) = - \alpha (1-softmax(x)[class])^gamma \log(softmax(x)[class])
-
-        The losses are averaged across observations for each minibatch.
-
-        Args:
-            alpha(1D Tensor, Variable) : the scalar factor for this criterion
-            gamma(float, double) : gamma > 0; reduces the relative loss for well-classiﬁed examples (p > .5), 
-                                   putting more focus on hard, misclassiﬁed examples
-            size_average(bool): By default, the losses are averaged over observations for each minibatch.
-                                However, if the field size_average is set to False, the losses are
-                                instead summed for each minibatch.
-
-
-    """
-    def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
-        super(FocalLoss, self).__init__()
-        if alpha is None:
-            self.alpha = torch.ones(class_num, 1)
-        else:
-            self.alpha = alpha
-        self.gamma = gamma
-        self.class_num = class_num
-        self.size_average = size_average
-
-    def forward(self, inputs, targets):
-        N = inputs.size(0)
-        C = inputs.size(1)
-        P = F.softmax(inputs)
-
-        class_mask = inputs.data.new(N, C).fill_(0)
-        ids = targets.view(-1, 1)
-        class_mask.scatter_(1, ids.data, 1.)
-        #print(class_mask)
-
-
-        if inputs.is_cuda and not self.alpha.is_cuda:
-            self.alpha = self.alpha.cuda()
-        alpha = self.alpha[ids.data.view(-1)]
-
-        probs = (P*class_mask).sum(1).view(-1,1)
-
-        log_p = probs.log()
-        #print('probs size= {}'.format(probs.size()))
-        #print(probs)
-
-        batch_loss = -alpha*(torch.pow((1-probs), self.gamma))*log_p 
-        #print('-----bacth_loss------')
-        #print(batch_loss)
-
-
-        if self.size_average:
-            loss = batch_loss.mean()
-        else:
-            loss = batch_loss.sum()
-        return loss
 """
 center loss, ECCV 2016, the full loss should be centerloss + softmaxloss    
 """     
